db4b732ab5cb84c367ad87c98f374ceda1d15b91
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2019-12-23 14:07:41 +0100

    Not marking gitOrgUrl as mandatory

--- releaser-core/src/main/java/releaser/internal/ReleaserProperties.java
+++ releaser-core/src/main/java/releaser/internal/ReleaserProperties.java
@@ -302,7 +302,6 @@ public class ReleaserProperties implements Serializable {
 		/**
 		 * The URL of the Git organization. We'll append each project's name to it.
 		 */
-		@NotBlank
 		private String gitOrgUrl;
 
 		/**
