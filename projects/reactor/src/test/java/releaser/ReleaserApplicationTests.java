36a8acc591696d8a378e3080ac41bc5992242c09
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-01-07 16:21:19 +0100

    Added reactor setup

--- projects/reactor/src/test/java/releaser/ReleaserApplicationTests.java
+++ projects/reactor/src/test/java/releaser/ReleaserApplicationTests.java
@@ -16,24 +16,47 @@
 
 package releaser;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import releaser.internal.options.Parser;
 import releaser.internal.spring.ExecutionResultHandler;
 import releaser.internal.spring.SpringReleaser;
+import releaser.internal.tasks.DryRunReleaseReleaserTask;
+import releaser.internal.tasks.ReleaserTask;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(
 		classes = { ReleaserApplicationTests.Config.class, ReleaserApplication.class },
 		properties = { "releaser.sagan.update-sagan=false" })
+@ActiveProfiles("test")
 class ReleaserApplicationTests {
+
+	@Autowired
+	ApplicationContext context;
 
 	@Test
 	void contextLoads() {
 
+	}
+
+	@Test
+	void should_load_reactor_version_of_publish_docs() {
+		Map<String, DryRunReleaseReleaserTask> beans = context
+				.getBeansOfType(DryRunReleaseReleaserTask.class);
+		List<ReleaserTask> inOrder = new LinkedList<>(beans.values());
+		inOrder.sort(AnnotationAwareOrderComparator.INSTANCE);
+		System.out.println(inOrder);
 	}
 
 	@Configuration
---
+++ projects/reactor/src/test/java/releaser/reactor/RestartSiteProjectPostReleaseTaskTests.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2013-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package releaser.reactor;
+
+import org.assertj.core.api.BDDAssertions;
+import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import releaser.internal.Releaser;
+import releaser.internal.ReleaserProperties;
+import releaser.internal.options.Options;
+import releaser.internal.project.ProjectVersion;
+import releaser.internal.project.Projects;
+import releaser.internal.spring.Arguments;
+import releaser.internal.spring.ExecutionResult;
+import releaser.internal.spring.ProjectToRun;
+import releaser.internal.spring.ProjectsFromBom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = RestartSiteProjectPostReleaseTaskTests.Config.class)
+@ActiveProfiles("test")
+class RestartSiteProjectPostReleaseTaskTests {
+
+	@Autowired
+	RestartSiteProjectPostReleaseTask task;
+
+	@Autowired
+	CfClient cfClient;
+
+	@Test
+	void should_update_the_website() {
+		Arguments arguments = Arguments.forProject(reactorCoreProject());
+
+		ExecutionResult result = task.runTask(arguments);
+
+		BDDAssertions.then(result.isSuccess()).isTrue();
+		BDDMockito.then(this.cfClient).should()
+				.restartApp(BDDMockito.eq("projectreactor"));
+	}
+
+	@Test
+	void should_not_update_the_website_if_project_not_reactor_core() {
+		Arguments arguments = Arguments.forProject(nonReactorCoreProject());
+
+		ExecutionResult result = task.runTask(arguments);
+
+		BDDAssertions.then(result.isSuccess()).isTrue();
+		BDDMockito.then(this.cfClient).shouldHaveNoInteractions();
+	}
+
+	private ProjectToRun reactorCoreProject() {
+		return new ProjectToRun(null,
+				new ProjectsFromBom(new Projects(), new ProjectVersion("foo", "1.0.0")),
+				new ProjectVersion("foo", "1.0.0"), new ReleaserProperties(),
+				BDDMockito.mock(Options.class)) {
+			@Override
+			public String name() {
+				return "reactor-core";
+			}
+		};
+	}
+
+	private ProjectToRun nonReactorCoreProject() {
+		return new ProjectToRun(null,
+				new ProjectsFromBom(new Projects(), new ProjectVersion("foo", "1.0.0")),
+				new ProjectVersion("foo", "1.0.0"), new ReleaserProperties(),
+				BDDMockito.mock(Options.class)) {
+			@Override
+			public String name() {
+				return "whatever";
+			}
+		};
+	}
+
+	@Configuration
+	@EnableAutoConfiguration
+	static class Config extends ReactorConfiguration {
+
+		@Bean
+		Releaser mockReleaser() {
+			return BDDMockito.mock(Releaser.class);
+		}
+
+		@Bean
+		CloudFoundryOperations mockCloudFoundryOperations() {
+			return BDDMockito.mock(CloudFoundryOperations.class);
+		}
+
+		@Override
+		CfClient cfClient(CloudFoundryOperations cloudFoundryOperations) {
+			return BDDMockito.mock(CfClient.class);
+		}
+
+	}
+
+}
