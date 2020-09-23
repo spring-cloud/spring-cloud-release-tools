74f0afe1a30e74c8aa70dc531bc6817e9b035227
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-09-23 12:45:31 +0200

    Picking original project version parsed from pom not from a folder

--- releaser-spring/src/main/java/releaser/internal/spring/VersionsToBumpFactory.java
+++ releaser-spring/src/main/java/releaser/internal/spring/VersionsToBumpFactory.java
@@ -65,9 +65,9 @@ class VersionsToBumpFactory implements Closeable {
 		ProjectVersion originalVersion = new ProjectVersion(project);
 		Projects fixedVersions = this.releaser.fixedVersions();
 		log.info("Got the following fixed versions [{}]", fixedVersions);
-		String fixedVersionForProject = fixedVersions.containsProject(project.getName())
+		String fixedVersionForProject = fixedVersions.containsProject(originalVersion.projectName)
 				? fixedVersions.forName(project.getName()).version : "";
-		log.info("Found fixed version for this project [{}] is [{}]", project.getName(), fixedVersionForProject);
+		log.info("Found fixed version for this project [{}] is [{}]", originalVersion.projectName, fixedVersionForProject);
 		ProjectVersion versionFromBom = StringUtils.hasText(fixedVersionForProject)
 				? new ProjectVersion(originalVersion.projectName, fixedVersionForProject)
 				: new ProjectVersion(project);
