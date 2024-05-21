620e41ae90c146a7b5cc77a6e99ade1776d7977b
Ryan Baxter <524254+ryanjbaxter@users.noreply.github.com>
2024-05-21 15:44:29 -0400

    Using host key property

--- releaser-core/src/main/java/releaser/internal/project/ProjectCommandExecutor.java
+++ releaser-core/src/main/java/releaser/internal/project/ProjectCommandExecutor.java
@@ -343,7 +343,7 @@ class CommandPicker {
 		String repo = properties.getGit().getOrgName() + "/" + version.projectName;
 		// TODO this needs to be a property
 		return "./jenkins.sh --docs-username " + properties.getAntora().getSpringDocsSshUsername() + " --docs-ssh-key-path " + properties.getAntora().getSpringDocsSshKeyPath()
-				+ " --docs-host docs-ip.spring.io --docs-ssh-host-key # --site-path "
+				+ " --docs-host docs-ip.spring.io --docs-ssh-host-key " + properties.getAntora().getSpringDocsSshHostKey() + " --site-path "
 				+ project.getAbsolutePath() + "/target/antora/site --github-repository " + repo + " --dry-run";
 	}
 
