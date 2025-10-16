aa1c3b868fb528d35e0dabaf77fad861b7cb1613
Ryan Baxter <ryan.baxter@broadcom.com>
2025-10-16 11:26:04 -0400

    Mark commercial antora docs unstable if file transfer fails

--- releaser-core/src/main/java/releaser/internal/docs/CommercialAntoraDocsPublisher.java
+++ releaser-core/src/main/java/releaser/internal/docs/CommercialAntoraDocsPublisher.java
@@ -61,13 +61,15 @@ public class CommercialAntoraDocsPublisher implements AntoraDocsPublisher {
 	public void publish(File project, ReleaserProperties properties) throws IOException {
 		checkAndLog();
 		String antoraSite = project.getAbsolutePath() + "/target/antora/site";
-		// This UploadBlobInfoFactory is used to alter the paths where the files will be uploaded in the bucket
+		// This UploadBlobInfoFactory is used to alter the paths where the files will be
+		// uploaded in the bucket
 		ParallelUploadConfig.UploadBlobInfoFactory uploadBlobInfoFactory = (String bucketName, String fileName) -> {
 			ProjectVersion version = new ProjectVersion(project);
-			// The spring-cloud-release docs will be under the project spring-cloud-starter-build but we want
+			// The spring-cloud-release docs will be under the project
+			// spring-cloud-starter-build but we want
 			// them uploaded under spring-cloud-release
-			String projectName = "spring-cloud-starter-build".equals(version.projectName) ? "spring-cloud-release" :
-					version.projectName;
+			String projectName = "spring-cloud-starter-build".equals(version.projectName) ? "spring-cloud-release"
+					: version.projectName;
 			String blobLocation = projectName + "/reference" + fileName.substring(antoraSite.length());
 			Optional<MediaType> mediaTypeOptional = MediaTypeFactory.getMediaType(blobLocation);
 			BlobInfo.Builder blobInfoBuilder = BlobInfo.newBuilder(bucketName, blobLocation);
